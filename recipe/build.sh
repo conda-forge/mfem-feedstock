55e694e6edfcc24a873045a8de21fbe0842e9cc6
Min RK <benjaminrk@gmail.com>
2024-06-04 14:56:24 +0200

    remove unneeded ompi env

--- recipe/build.sh
+++ recipe/build.sh
@@ -18,12 +18,5 @@ make lib -j${CPU_COUNT}
 make install
 
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
-  if [[ "$mpi" == "openmpi" ]]; then
-    export OMPI_MCA_plm=isolated
-    export OMPI_MCA_btl_vader_single_copy_mechanism=none
-    export OMPI_MCA_rmaps_base_oversubscribe=yes
-    export OMPI_ALLOW_RUN_AS_ROOT=1
-    export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
-  fi
   make check
 fi
